643ec6cfec90ab49e479f6f42c64eb8bba575980
Piergiorgio <piergiorgio.deprosperis@mashfrog.com>
2018-11-26 15:09:37 +0100

    cose

--- sites/all/themes/photorent/templates/node--home.tpl.php
+++ sites/all/themes/photorent/templates/node--home.tpl.php
@@ -11,10 +11,7 @@ $user_full = user_load($user->uid); // user_load(uid) returns the complete array
 </p>
 <p>Hai ancora <?php echo("N.Giorni "); ?> giorni per aggiungere extra alla tua mostra.</p>
 <div>
-  <h2><a href="">Le mie mostre</a></h2>
-</div>
-<div>
-  <h2><a href="">Extra</a></h2>
+  <h2><a href="">Le tue mostre</a></h2>
 </div>
 <div>
   <h2><a href="">Offerte</a></h2>
@@ -31,7 +28,7 @@ $user_full = user_load($user->uid); // user_load(uid) returns the complete array
 <p>Ciao <?php echo($user_full->field_nome_e_cognome['und'][0]['value']); ?>
 </p>
 <div>
-  <h2><a href="">Le mie mostre</a></h2>
+  <h2><a href="">Le tue mostre</a></h2>
 </div>
 <div>
   <h2><a href="">Tutte le mostre</a></h2>
