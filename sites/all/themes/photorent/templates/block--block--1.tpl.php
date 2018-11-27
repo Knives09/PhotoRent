4df60abf61e6cfd743e93110f8b156a7d1cb8ba7
Piergiorgio <piergiorgio.deprosperis@mashfrog.com>
2018-11-27 11:14:14 +0100

    header e footer con menu

--- sites/all/themes/photorent/templates/block--block--1.tpl.php
+++ sites/all/themes/photorent/templates/block--block--1.tpl.php
@@ -1,2 +1,25 @@
-<?php 
-echo("<h1>Blocco header</h1>"); ?>
+<?php
+global $base_url;
+  $menu=menu_tree_all_data("main-menu", $link = NULL, $max_depth = NULL);
+?>
+<div class="">
+    <div class="">
+      <div class="">
+        <ul>
+        <?php
+          foreach ($menu as $key) { ?>
+            <li>
+              <a href="<?php echo($key['link']['href']); ?>"><?php echo($key['link']['link_title']); ?></a>
+            <?php if (isset($key['below'])){ ?>
+              <ul>
+                <?php foreach ($key['below'] as $value) { ?>
+                <li><a href="<?php print ($value['link']['link_path']); ?>"><?php print($value['link']['link_title']); ?></a></li>
+                <?php } ?>
+              </ul>
+            <?php } ?>
+            </li>
+        <?php }?>
+        </ul>
+      </div>
+    </div>
+  </div>
--- sites/all/themes/photorent/templates/block--block--2.tpl.php
+++ sites/all/themes/photorent/templates/block--block--2.tpl.php
@@ -1,2 +1,35 @@
-<?php 
-echo("<h1>Blocco footer</h1>"); ?>
+<?php
+global $base_url;
+  $menu=menu_tree_all_data("main-menu", $link = NULL, $max_depth = NULL);
+?>
+<footer class="footer">
+  <div class="container">
+    <nav class="nav nav-footer">
+      <ul>
+      <?php
+        foreach ($menu as $key) { ?>
+          <li>
+            <a href="<?php echo($key['link']['href']); ?>"><?php echo($key['link']['link_title']); ?></a>
+          <?php if (isset($key['below'])){ ?>
+            <ul>
+              <?php foreach ($key['below'] as $value) { ?>
+              <li><a href="<?php print ($value['link']['link_path']); ?>"><?php print($value['link']['link_title']); ?></a></li>
+              <?php } ?>
+            </ul>
+          <?php } ?>
+          </li>
+      <?php }
+       ?>
+      </ul>
+    </nav>
+    <div class="footer-bottom">
+      <div class="logo-footer">
+        <img src="" alt="">
+        <span>Fotorent</span>
+      </div>
+      <div class="privacy-social">
+        <span>Privacy note legali lorem etc etc</span>
+      </div>
+    </div>
+  </div>
+</footer>
