3eff39eae106717b133ec99f2bc0e16a8fd493c6
Piergiorgio <piergiorgio.deprosperis@mashfrog.com>
2018-12-11 12:46:40 +0100

    button

--- sites/all/themes/photorent/templates/webform-form-5.tpl.php
+++ sites/all/themes/photorent/templates/webform-form-5.tpl.php
@@ -1,22 +1,23 @@
 <div class="container">
-	<div class="row">
-		<div class="form-opere col-md-12 col-lg-12">
-			<p class="title-opere">Inviaci le tue opere</p>
-			<ul>
-				<li>
-				    <?php print drupal_render($form['submitted']['nome']); ?>
-				</li><li>
-				    <?php print drupal_render($form['submitted']['cognome']); ?>
-				</li><li>
-				    <?php print drupal_render($form['submitted']['mail']); ?>
-				</li><li>
-				    <?php print drupal_render($form['submitted']['telefono']); ?>
-				</li><li>
-				    <?php print drupal_render($form['submitted']['opere']); ?>
-				</li><li>
-				    <?php print drupal_render($form['submitted']['privacy']); ?>
-				</li>
-			</ul>
-		</div>
-	</div>
+  <div class="row">
+    <div class="form-opere col-md-12 col-lg-12">
+      <p class="title-opere">Inviaci le tue opere</p>
+      <ul>
+        <li>
+            <?php print drupal_render($form['submitted']['nome']); ?>
+        </li><li>
+            <?php print drupal_render($form['submitted']['cognome']); ?>
+        </li><li>
+            <?php print drupal_render($form['submitted']['mail']); ?>
+        </li><li>
+            <?php print drupal_render($form['submitted']['telefono']); ?>
+        </li><li>
+            <?php print drupal_render($form['submitted']['opere']); ?>
+        </li><li>
+            <?php print drupal_render($form['submitted']['privacy']); ?>
+        </li>
+        <li> <?php print drupal_render($form['actions']); ?></li>
+      </ul>
+    </div>
+  </div>
 </div>
